0947c451f6947d6496804b5d0064e408a3b75163
xudong_yan <xudong_yan@hotmail.com>
2023-12-14 20:15:26 +0200

    fix a sql sytax error

--- app/services/DataImportService.js
+++ app/services/DataImportService.js
@@ -1,7 +1,7 @@
 import {sql} from "../database/database.js";
 
 const getSomeDBEntries = async () => {
-    return await sql `SELECT TOP 10 * FROM Journeys`;
+    return await sql `SELECT * FROM Journeys LIMIT 10`;
 };
 
 export { getSomeDBEntries };
